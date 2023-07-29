bf4ff899def9c684f86bf5b2dda9477db813806b
Vaibhav <Vaibhav@SEZLAP-14703.india.rsystems.com>
2023-07-29 20:23:40 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,21 @@
+public class Test {
+
+	public static void main(String[] args) {
+	
+       Testing.show();
+       Testing t= new Testing();
+       t.display();
+	}
+
+	static void show()
+	{
+		
+		System.out.println("I am showing");
+	}
+	
+	void display()
+	{
+		
+		System.out.println("I am displaying");
+	}
+}
